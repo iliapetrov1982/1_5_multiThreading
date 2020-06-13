5c30e0d78d9070e7482d9a0b35deda53b816f034
iliapetrov1982 <ilia-p@yandex.ru>
2020-06-13 16:08:33 +0300

    monitors implemented

--- src/task1/Foo.java
+++ src/task1/Foo.java
@@ -1,17 +1,45 @@
 package task1;
 
 public class Foo {
+    boolean valueSet = false;
+    boolean valueSet1 = false;
     private Object key = new Object();
-        public void first () {
+
+    public synchronized void first() {
+        while (valueSet)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        valueSet = true;
         System.out.print("first");
+        notifyAll();
     }
 
-        public void second () {
+    public synchronized void second() {
+        while (!valueSet)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        valueSet = false;
         System.out.print("second");
+        notify();
+        valueSet = false;
+        valueSet1 = true;
     }
 
-        public void third () {
+
+    public synchronized void third() {
+        while (!valueSet1)
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         System.out.print("third");
+        valueSet1 = false;
     }
-
 }
